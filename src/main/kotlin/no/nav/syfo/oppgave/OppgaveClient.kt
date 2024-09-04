6d864441182955c5d2f211c8319e2b99f9684774
Andreas Sagen Aspaas <danduras@gmail.com>
2024-09-04 15:22:36 +0200

    feat: return oppgaveId if possible

--- src/main/kotlin/no/nav/syfo/oppgave/OppgaveClient.kt
+++ src/main/kotlin/no/nav/syfo/oppgave/OppgaveClient.kt
@@ -2,9 +2,12 @@ package no.nav.syfo.oppgave
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.get
 import io.ktor.client.request.header
+import io.ktor.client.request.parameter
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
@@ -12,9 +15,11 @@ import io.ktor.http.contentType
 import java.time.LocalDate
 import no.nav.syfo.azuread.AccessTokenClientV2
 import no.nav.syfo.utils.logger
+import java.util.UUID
 
 interface OppgaveClient {
     suspend fun opprettOppgave(opprettOppgave: OpprettOppgave): OpprettOppgaveResponse
+    suspend fun getOppgaveId(journalpostId: String): OppgaveResponse
 }
 
 class OppgaveClientProduction(
@@ -42,12 +47,41 @@ class OppgaveClientProduction(
             )
         }
     }
+
+    override suspend fun getOppgaveId(journalpostId: String): OppgaveResponse {
+        val accessToken = accessTokenClientV2.getAccessTokenV2(scope)
+
+        val response: HttpResponse =
+            httpClient.get(url) {
+                header("Authorization", "Bearer $accessToken")
+                header("X-Correlation-ID", UUID.randomUUID().toString())
+                parameter("tema", "SYM")
+                parameter("oppgavetype", "JFR")
+                parameter("journalpostId", journalpostId)
+                parameter("statuskategori", "AAPEN")
+                parameter("sorteringsrekkefolge", "ASC")
+                parameter("sorteringsfelt", "FRIST")
+                parameter("limit", "10")
+            }
+
+        return response.body<OppgaveResponse>()
+    }
 }
 
 class DevelopmentOppgaveClient : OppgaveClient {
     override suspend fun opprettOppgave(opprettOppgave: OpprettOppgave): OpprettOppgaveResponse {
         logger.info("later som vi oppretter oppgave for ${opprettOppgave.journalpostId}")
         return OpprettOppgaveResponse(1, 1)
+    }
+
+    override suspend fun getOppgaveId(journalpostId: String): OppgaveResponse {
+        return OppgaveResponse(
+            antallTreffTotalt = 2,
+            listOf(
+                Oppgave(1),
+                Oppgave(2),
+            )
+        )
     }
 }
 
@@ -77,4 +111,15 @@ data class OpprettOppgaveResponse(
     val status: String? = null,
     val tildeltEnhetsnr: String? = null,
     val mappeId: Int? = null,
+)
+
+
+
+data class OppgaveResponse(
+    val antallTreffTotalt: Int,
+    val oppgaver: List<Oppgave>,
+)
+
+data class Oppgave(
+    val id: Int,
 )
