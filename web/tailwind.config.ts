b723bc0cf08f4d6afc7e8e568feda450738ed42c
Karl J. Overå <k@rl.run>
2025-01-20 10:08:07 +0100

    chore: use tailwind.config.ts

---
+++ web/tailwind.config.ts
@@ -0,0 +1,11 @@
+import naviktTailwindPreset from '@navikt/ds-tailwind'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    presets: [naviktTailwindPreset],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
